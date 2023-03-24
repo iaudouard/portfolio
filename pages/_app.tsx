0f59da7d99c54ae6e49b752ccb26edd69a3f4ac2
Ivan Audouard <i.audouard22@ejm.org>
2023-03-24 14:24:12 +0900

    new font + restyled for simplicity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-import Nav from "../components/header";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Nav />
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import { Hero } from "../components/hero";
+import localFont from "next/font/local";
+
+const romanticaStd = localFont({
+  src: "../public/assets/fonts/romantica.otf",
+});
+
+const Link = ({ href, label, aria }) => {
+  return (
+    <a href={href} target="_blank" rel="noreferrer" aria-label={aria}>
+      <h4 className="text-3xl underline text-blue-500">{label}</h4>
+    </a>
+  );
+};
 
 const Home: NextPage = () => {
   return (
@@ -15,8 +27,30 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen w-screen overflow-x-hidden">
-        <Hero />
+      <main className={"min-h-screen p-8 " + romanticaStd.className}>
+        <h1 className="text-6xl sm:text-8xl md:text-9xl ">Ivan Audouard</h1>
+        <div>
+          <Link
+            href="mailto:i@ivanaudouard.com"
+            label="Email"
+            aria="mailto link"
+          />
+          <Link
+            href="https://github.com/iaudouard"
+            label="Github"
+            aria="link to github"
+          />
+          <Link
+            href="https://www.linkedin.com/in/ivan-audouard-8073b7211/"
+            label="LinkedIn"
+            aria="link to linkedin"
+          />
+          <Link
+            href="/assets/Ivan_Audouard_CV.pdf"
+            label="CV"
+            aria="link to download cv"
+          />
+        </div>
       </main>
     </>
   );
